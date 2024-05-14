7fe39dd4578530a7768a544af6ae71aee872b182
Sayan Mallick <snyxmk@gmail.com>
2024-05-14 16:06:14 +0530

    stage 3

--- app/server.go
+++ app/server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"net"
+	"net/textproto"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,11 +17,46 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		go func(conn net.Conn) {
+			buf := make([]byte, 1024)
+			_, err := conn.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading: ", err.Error())
+				return
+			}
+
+			reader := bufio.NewReader(bytes.NewReader(buf))
+			tp := textproto.NewReader(reader)
+			requestLine, err := tp.ReadLine()
+			if err != nil {
+				fmt.Println("Error reading: ", err.Error())
+				return
+			}
+
+			fields := strings.Split(string(requestLine), " ")
+			if len(fields) < 2 {
+				fmt.Printf("invalid request line: %s\n", string(requestLine))
+				return
+			}
+			urlPath := fields[1]
+
+			if urlPath == "/" {
+				fmt.Printf("Url Path: %s\n", urlPath)
+				conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			} else {
+				fmt.Printf("Url Path: %s\n", urlPath)
+				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			}
+
+			conn.Close()
+		}(conn)
+
+	}
 }
